a2690c345fcfdb9b8acb1c9990946c251eb5adbd
Hiraisun <koushi.2nd@gmail.com>
2024-11-12 15:29:05 +0900

    refacter: appディレクトリを整理
    /frontend/app で作成していたページを、 /frontend/src/app に統合

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+import Link from "next/link";
 
+// Home : ダッシュボードとか？
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex h-screen">
+      <Left />
+      <Right />
     </div>
+  );
+}
+
+
+function Left() {
+  return (
+    <div className="w-1/2 bg-gray-200">
+      <div className="p-8">
+        <h2 className="text-5xl font-semibold">Challange</h2>
+        <p>挑戦できるコンテンツを並べていくところ 継続は力なりhogehoge</p>
+      </div>
+      <ChallangeCardList />
+    </div>
+  );
+}
+
+function Right() {
+  return (
+    <div className="w-1/2 bg-gray-100 p-8 hover-scroll">
+      <h2 className="text-5xl font-semibold">History</h2>
+      <p>ダッシュボードてきな。履歴、直近のスコアなど。ここには簡易表示で、別に詳細ページつくる？</p>
+    </div>
+  );
+}
+
+function ChallangeCardList() {
+  const cards = [
+    { id: 0, title: "デイリーチャレンジ" },
+    { id: 1, title: "常設チャレンジ" },
+    { id: 2, title: "○○チャレンジ" },
+    { id: 3, title: "○○チャレンジ" },
+    { id: 4, title: "○○チャレンジ" },
+    { id: 5, title: "○○チャレンジ" },
+  ];
+
+  return (
+    <div className="overflow-y-auto h-full hover-scroll p-8">
+      <div className="space-y-4">
+        {cards.map((card) => (
+          <ChallangeCard key={card.id} id={card.id} title={card.title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ChallangeCard({ id, title }: { id: number; title: string }) {
+  return (
+    <Link href={`/challenge/${id}`} className="block bg-white p-4 h-48
+    rounded-md shadow-lg shadow-gray-500/50 
+    transition-transform transform hover:scale-105 hover:shadow-2xl">
+      <h3 className="text-xl font-bold">{title}</h3>
+      <p>ID: {id}</p>
+      <p>description</p>
+    </Link>
   );
 }
