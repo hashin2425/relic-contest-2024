e7c3bd994a3c6080a540ddb3cab7ea312f04bbd6
Haya <hayachannel5@gmail.com>
2024-12-02 14:48:49 +0900

    feat: 正しくチャレンジ・画像が取得できる状態にする

--- backend/app/core/mongodb_core.py
+++ backend/app/core/mongodb_core.py
@@ -1,13 +1,18 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
+""" MongoDBを操作するクラス """
+
+from datetime import datetime
 from typing import List
 import json
-from datetime import datetime
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.models.mongodb_models import Challenge
+from app.core.exceptions import ServiceUnavailableError
 
 
 class MongoDB:
+    """MongoDBを操作するクラス"""
+
     def __init__(self):
         self.client = None
         self.db = None
@@ -33,6 +38,9 @@ class MongoDB:
 
     async def init_challenges(self):
         """初期チャレンジデータの読み込みと保存"""
+        if self.db is None or self.db.challenges is None:
+            return
+
         if await self.db.challenges.count_documents({}) > 0:
             return
 
@@ -48,16 +56,23 @@ class MongoDB:
 
     async def get_all_challenges(self) -> List[Challenge]:
         """全チャレンジを取得"""
+        if self.db is None or self.db.challenges is None:
+            raise ServiceUnavailableError("Could not connect to the service")
+
         cursor = self.db.challenges.find()
         challenges = await cursor.to_list(length=None)
         return [Challenge(**challenge) for challenge in challenges]
 
     async def get_challenge_by_id(self, challenge_id: str) -> Challenge:
         """IDによるチャレンジ取得"""
+        if self.db is None or self.db.challenges is None:
+            raise ServiceUnavailableError("Could not connect to the service")
+
         challenge = await self.db.challenges.find_one({"_id": challenge_id})
-        if challenge:
-            return Challenge(**challenge)
-        return None
+        if not challenge:
+            raise ServiceUnavailableError("Could not find the challenge")
+
+        return Challenge(**challenge)
 
 
 # グローバルなMongoDBインスタンス
